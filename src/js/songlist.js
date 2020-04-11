06155be38ebf5e1ecc9298e9166647d7fa6109fe
Jin Cheng <jinchengjoker@foxmail.com>
2020-04-11 18:30:00 +0800

    songlist render by data

--- src/js/songlist.js
+++ src/js/songlist.js
@@ -1,28 +1,51 @@
 {
-  const model = {}
+  const model = {
+    data: {
+      songList: [
+        {
+          id: 1,
+          song: '七里香',
+          singer: '周杰伦'
+        },
+        {
+          id: 2,
+          song: '一路向北',
+          singer: '周杰伦'
+        },
+        {
+          id: 3,
+          song: '枫',
+          singer: '周杰伦'
+        }
+      ]
+    }
+  }
   const view = {
     el: 'aside',
     template: `
       <div id="asideTitle" class="bg-light text-center">歌曲列表</div>
       <div id="asideSongList">
-        <div class="list-group list-group-flush">
-          <button type="button" class="list-group-item list-group-item-action">Cras justo odio</button>
-          <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
-          <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
-          <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
-          <button type="button" class="list-group-item list-group-item-action">Vestibulum at eros</button>
-        </div>
+        <ul class="list-group list-group-flush"></ul>
       </div>
     `,
     render(data) {
       $(this.el).html(this.template)
+      data.songList.map(item => {
+        $(`
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            ${item.song}
+            <span>${item.singer}</span>
+          </li>
+        `)
+          .appendTo($(this.el).find('ul.list-group'))
+      })
     }
   }
   const controller = {
     init(model, view) {
       this.model = model
       this.view = view
-      this.view.render()
+      this.view.render(this.model.data)
     }
   }
 
