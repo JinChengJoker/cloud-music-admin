86f1d829bcf392f33d9aefd350ddcbaf79edc7e9
JinCheng <JinChengJoker@foxmail.com>
2020-04-30 17:15:44 +0800

    add upload progress

--- src/js/upload.js
+++ src/js/upload.js
@@ -10,10 +10,34 @@
           <p>单个文件最大限制 10M</p>
           <input type="file">
         </div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"
+            style="width: 0%">
+          </div>
+        </div>
       </div>
     `,
     render(data) {
       $(this.el).html(this.template)
+    },
+    showProgress() {
+      $('.progress').css('opacity', 1)
+    },
+    hideProgress() {
+      $('.progress').css('opacity', 0)
+    },
+    resetProgress() {
+      this.hideProgress()
+      $('.progress-bar')
+        .attr('aria-valuenow', 0)
+        .css('width', '0%')
+    },
+    updateProgress(value) {
+      $('.progress-bar')
+        .attr('aria-valuenow', value)
+        .css('width', `${value}%`)
     }
   }
 
@@ -42,21 +66,30 @@
     },
 
     uploadWithSDK(token, putExtra, config) {
-      $("#dragArea input[type='file']").unbind("change").bind("change", function () {
-        var file = this.files[0]
+      $("#dragArea input[type='file']").unbind("change").bind("change", (e) => {
+        var file = e.currentTarget.files[0]
         var observable
         if (file) {
+          this.view.showProgress()
           var key = file.name
           putExtra.params["x:name"] = key.split(".")[0]
           // 设置next,error,complete对应的操作，分别处理相应的进度信息，错误信息，以及完成后的操作
-          var next = function (response) { }
-          var complete = function (res) {
-            window.eventhub.emit('upload', {
-              song: res['x:name'],
-              url: `http://q8gllkohk.bkt.clouddn.com/${encodeURI(res.key)}`
-            })
+          var next = (response) => {
+            this.view.updateProgress(response.total.percent)
+          }
+          var complete = (res) => {
+            setTimeout(() => {
+              this.view.resetProgress()
+              $(e.currentTarget).val('')
+              window.eventhub.emit('upload', {
+                song: res['x:name'],
+                url: `http://q8gllkohk.bkt.clouddn.com/${encodeURI(res.key)}`
+              })
+            }, 500);
           }
           var error = function (err) {
+            this.view.resetProgress()
+            $(e.currentTarget).val('')
             alert("upload error:" + JSON.stringify(err))
           }
           var subObject = {
